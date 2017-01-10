7b59bae556562991ba1c8c12cad28d38d60a584d
Jacob Bleser <megaboy@localhost.localdomain>
2017-01-09 19:37:01 -0500

    Added icons, removed random images, partial text written, misc optimizations

--- js/index.js
+++ js/index.js
@@ -2,7 +2,3 @@ console.log("Connected");
 
 // Import css
 require('../public/css/main.scss');
-
-// Import images
-var img = document.createElement('img');
-img.src = require("../public/assets/dark-background.png");
